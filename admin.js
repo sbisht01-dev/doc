483530af8a611a220aa6b6e590ff5814be63fa6b
Saurabh Bisht <76562057+sbisht01-dev@users.noreply.github.com>
2024-04-17 10:40:15 +0530

    auth and signout function

--- admin.js
+++ admin.js
@@ -1,11 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-analytics.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
 import {
-  getDatabase,
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import {
+  getStorage,
   ref,
-  onValue,
-} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
-// import { QueryConstraint } from "firebase/database";
+  listAll,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-storage.js";
 
 const newKey = "AIzaSyDvlSSduzGaNjaaYkSHTOdcVnlRBd39j5U";
 const firebaseConfig = {
@@ -19,33 +25,128 @@ const firebaseConfig = {
   appId: "1:628507138263:web:ac1eec54ae6207685d7892",
   measurementId: "G-LCQ4KW3JKY",
 };
+let container = document.getElementById("container");
+const userUID = localStorage.getItem("uid");
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const analytics = getAnalytics(app);
 const database = getDatabase(app);
-const userInformation = ref(database, "UID");
-let currentUID = localStorage.getItem("userID");
-let verified = false;
+const storage = getStorage(app);
+const storageRef = ref(storage, "gs://dimension01-7a7b4.appspot.com");
+const fileNames = [];
+const fileURLs = [];
 
-onValue(userInformation, function (snapshot) {
-  let uidObject = snapshot.val();
-  for (const key in uidObject) {
-    const value = uidObject[key];
-    userVerification(currentUID, value);
-  }
-  if (verified == false) {
-    window.location.href = "signin.html";
-  }
-});
-
-function adminVerify() {
-  const queryParams = new URLSearchParams(window.location.search);
-  const uid = queryParams.get("query");
+function getFileData() {
+  // gets all data of file
+  let j = 0;
+  listAll(storageRef)
+    .then((res) => {
+      res.items.forEach((itemRef) => {
+        fileNames.push(removeExtension(itemRef._location.path));
+        webpageDOMName(fileNames, j);
+        j++;
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  let i = 0;
+  //gets URL of the file in storage
+  listAll(storageRef)
+    .then((res) => {
+      res.items.forEach((itemRef) => {
+        getDownloadURL(itemRef)
+          .then((url) => {
+            fileURLs.push(url);
+            webpageDOM(fileURLs, i);
+            i++;
+          })
+          .catch((error) => {
+            console.error("Error getting download URL:", error);
+          });
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-function userVerification(currentUID, uidValue) {
-  if (currentUID == uidValue) {
-    verified = true;
+function webpageDOMName(fileNames, j) {
+  let fileView = document.getElementById("files");
+  fileView.innerHTML += `
+    <a id="${fileNames[j]}" target="_blank"
+    href=""><div class="header">
+        <p class="name"> ${fileNames[j]}  </p>
+        <div class="button">Open</div>
+</div> </a>`;
+}
+
+function webpageDOM(fileURLs, i) {
+  1;
+  const fileID = document.getElementById(`${fileNames[i]}`);
+  const fileNameWithoutExtension = decodeURIComponent(
+    fileURLs[i].substring(
+      fileURLs[i].lastIndexOf("/") + 1,
+      fileURLs[i].indexOf("?")
+    )
+  ).replace(/\.[^.]+$/, "");
+
+  const filesContainer = document.getElementById("files");
+
+  const anchorTags = filesContainer.querySelectorAll("a");
+
+  // Iterate over each anchor tag and update the href of the matching one
+  anchorTags.forEach((anchorTag) => {
+    if (anchorTag.id === fileNameWithoutExtension) {
+      anchorTag.href = fileURLs[i];
+    }
+  });
+}
+
+//fix file name
+function removeExtension(filename) {
+  const lastDotIndex = filename.lastIndexOf(".");
+  if (lastDotIndex !== -1) {
+    return filename.substring(0, lastDotIndex);
+  } else {
+    return filename;
   }
 }
+
+function userDetail() {
+  let userMail = localStorage.getItem("user");
+  const currentUser = document.getElementById("user");
+  currentUser.innerHTML = `Account: ${userMail}`;
+}
+
+function logoutUser() {
+  auth
+    .signOut()
+    .then(() => {
+      // Sign-out successful, redirect to the signin page
+      window.location.href = "signin.html";
+    })
+    .catch((error) => {
+      console.error("Error signing out:", error);
+    });
+}
+
+let loginBtn = document.getElementById("logout");
+loginBtn.addEventListener("click", logoutUser);
+
+// Function to check authentication state
+function checkAuthentication() {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log(user);
+      // User is authenticated, continue with page logic
+      getFileData();
+      userDetail();
+    } else {
+      // User is not authenticated, redirect to the signin page
+      window.location.href = "signin.html";
+    }
+  });
+}
+checkAuthentication();
